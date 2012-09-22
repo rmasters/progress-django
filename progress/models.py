f2ed83de1844fc4e1260318aa9affc64ad7ddf61
Ross Masters <ross@rossmasters.com>
2012-09-22 23:35:16 +0100

    Added value/unit concepts as well as counting down

--- progress/models.py
+++ progress/models.py
@@ -3,33 +3,82 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.contrib import admin
 
+from django.utils.timezone import now
+import time
+
 class Goal(models.Model):
-	name = models.CharField(max_length=50)
-	addedAt = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    addedAt = models.DateTimeField(auto_now_add=True)
+    unit = models.CharField(max_length=10)
+    targetValue = models.FloatField()
+    targetDate = models.DateTimeField(null=True, blank=True)
+    targetIsLess = models.BooleanField(default=False)
 
-	def save(self, progress=0, *args, **kwargs):
-		super(Goal, self).save(*args, **kwargs)
-	
-		if self.pk is None:
-			# Create the current status (0, if not given)
-			progression = Progression(goal=self, completed=progress)
-			progression.save()
-	
-	def status(self):
-		if len(self.progression_set.all()) == 0:
-			return Progression()
-		return sorted(self.progression_set.all(), key=lambda k: k.updatedAt, reverse=True)[0]
+    class Meta:
+        get_latest_by = 'addedAt'
 
-	def __unicode__(self):
-		return "%s: %s" % (self.name, self.status() if self.status() else '0%')
+    def save(self, *args, **kwargs):
+        super(Goal, self).save(*args, **kwargs)
 
-class Progression(models.Model):
-	goal = models.ForeignKey(Goal)
-	completed = models.FloatField(default=0.0)
-	updatedAt = models.DateTimeField(auto_now_add=True)
-	
-	def __unicode__(self):
-		return "%.0f%%" % round(self.completed, 2)
+    def status(self):
+        if len(self.update_set.all()) == 0:
+            return Update(goal=self)
+        return self.update_set.order_by('updatedAt').latest()
+
+    def percentage(self):
+        cur_value = self.update_set.order_by('updatedAt').latest().value
+
+        if self.targetIsLess is True:
+            cur_value = cur_value - self.targetValue
+        return (cur_value / self.targetValue) * 100
+
+    def hasTargetDate(self):
+        return self.targetDate is not None
+
+    """
+    Get remaining time available
+    Returns: datetime.timedelta
+
+    """
+    def remaining(self):
+        if not self.hasTargetDate(): return None
+        return self.targetDate - now()
+
+    def __unicode__(self):
+        return "%s: %s" % (self.name, self.status() if self.status() else '0%')
+
+    def target(self):
+        return Update(goal=self, value=self.targetValue)
+
+class Update(models.Model):
+    goal = models.ForeignKey(Goal)
+    value = models.FloatField(default=0.0)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        get_latest_by = 'updatedAt'
+
+    """
+    Some hard-coded formats for things like percentages
+
+    """
+    def format_unit(self, value, unit):
+        formats = {'%': '%s%%'}
+        if unit in formats:
+            return formats[unit] % value
+        return '%s %s' % (value, unit)
+
+    def get_float_or_int(self, value):
+        r = '%.2f' % value
+
+        rem = r[r.find('.')+1:]
+        if len(rem.rstrip('0')) == 0:
+            return r[:r.find('.')]
+        return r
+
+    def __unicode__(self):
+        val = self.get_float_or_int(self.value)
+        return self.format_unit(val, self.goal.unit)
 
 admin.site.register(Goal)
-admin.site.register(Progression)
+admin.site.register(Update)
--- progress/views.py
+++ progress/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from progress.models import Goal, Progression
+from progress.models import Goal, Update
 
 def index(request):
 	goals = Goal.objects.all()
